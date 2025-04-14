2053adc3fe3e18b1851d91d50c33153913331076
Marcos100Dalma <mdalma@mail.austral.edu.ar>
2025-04-14 12:53:45 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    age = int(input("Ingrese un año:"))
+    if age % 400 == 0 and age % 4 == 0:
+        print("El año " + str(age) + " es bisiesto")
+    else:
+        print("El año " + str(age) + " no es bisiesto")
